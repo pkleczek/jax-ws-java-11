b4d7163031de42676a258ea220b6412140245ea0
Paweł Kłeczek <pkleczek@gmail.com>
2023-03-20 18:54:53 +0100

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,36 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import eu.europa.ec.taxud.vies.services.checkvat.CheckVatPortType;
+import eu.europa.ec.taxud.vies.services.checkvat.CheckVatService;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.Holder;
+
+public class App {
+    public static void main(String[] args) throws DatatypeConfigurationException {
+        CheckVatService checkVatService = new CheckVatService();
+        CheckVatPortType checkVatPort = checkVatService.getCheckVatPort();
+
+        Holder<XMLGregorianCalendar> requestDate = new Holder<>();
+        Holder<Boolean> valid = new Holder<>();
+        Holder<String> name = new Holder<>();
+        Holder<String> address = new Holder<>();
+
+        checkVatPort.checkVat(
+            new Holder<>("PL"),
+            new Holder<>("5252352907"),
+            requestDate,
+            valid,
+            name,
+            address
+        );
+
+
+        System.out.println(requestDate.value);
+        System.out.println(valid.value);
+        System.out.println(name.value);
+        System.out.println(address.value);
     }
 }
